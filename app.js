7e6eb075a2c8ac12d77e54abafcf4ad726c23e92
Hasmukh Panchal <hasmukh@gmail.com>
2023-03-24 16:53:02 -0400

    Updated with mongoose objects and schemas and model

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+//Setting up MongoDB COnnections and it's values through process envirnment variables. 
+const srvURL = process.env.N1_URL || "127.0.0.1:27017";
+const dbUser = process.env.N1_KEY || "userAdmin";
+const dbPasswd = process.env.N1_SECRET || "userAdmin123";
+const dbName = process.env.N1_DB || "userDB";
+
+mongoose.set("strictQuery", false);
+
 const app = express();
 
 const port = 2933;
@@ -13,14 +21,39 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const mongoDB = "mongodb://"+dbUser+":"+dbPasswd+"@"+srvURL+"/"+dbName;
+//const mongoDB = 'mongodb+srv://'+dbUser+':'+dbPasswd+'@'+srvURL+'/'+dbName+'?retryWrites=true&w=majority';
+
+main().catch(err => console.log(err));
+async function main() {
+    //await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    try {
+      await mongoose.connect(mongoDB);  //if your database has auth enabled  
+    } catch (error) {
+      console.log(error);
+    }    
+};
+
+const userSchema = {
+    email : {
+        type: String,
+        required: [true, 'Can not allow null user email']
+    },
+    password: {
+        type: String,
+        required: [true, 'Can not allow null password']
+    }
+};
+
+const User = mongoose.Model("User",userSchema);
+
 //Render the Home page while someone hits the base URI
 app.get("/", async (req, res)=>{
     try {
         res.render("home");    
     } catch (error) {
         res.send(error);
-    }
-    
+    }    
 });
 
 //Render the Login page while someone hits the Login URI
@@ -33,7 +66,7 @@ app.get("/register", async (req, res)=>{
     res.render("register");
 });
 
-
+//Accept he login post page upon submissions
 
 
 
